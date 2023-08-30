e69e2ccdd37cee8e098a614cc128f8eeed5f975f
cunhavcs <cunha.vcs@gmail.com>
2023-08-30 10:57:18 -0300

    Req 01: implementa a funcao study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # Se o target_time é vazio, retornar None
+    if target_time is None:
+        return None
+
+    # Iniciar um contador zerado
+    presentes_no_horario = 0
+
+    # Para cada estudante da lista, verificar se está presente no target_time
+    for estudante in permanence_period:
+        # Se um elemento da tupla é inválido, retornar None
+        if type(estudante[0]) != int or type(estudante[1]) != int:
+            return None
+
+        # Se estiver presente, somar 1 ao contador
+        if estudante[0] <= target_time <= estudante[1]:
+            presentes_no_horario += 1
+
+    # Retornar o contador
+    return presentes_no_horario
