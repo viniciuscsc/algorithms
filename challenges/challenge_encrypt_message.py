f4191f1ec837fcab705c92931b4257cd0cbd591b
cunhavcs <cunha.vcs@gmail.com>
2023-08-30 12:04:02 -0300

    Req 02: implementa a funcao test_encrypt_message

--- challenges/challenge_encrypt_message.py
+++ challenges/challenge_encrypt_message.py
@@ -1,14 +1,26 @@
+# Recebe uma string message e um inteiro key como parâmetros
 def encrypt_message(message: str, key: int):
-
+    # Se key não possui o tipo correto, uma exceção é lancada
     if not isinstance(key, int):
         raise TypeError("tipo inválido para key")
 
+    # Se message não possui o tipo correto, uma exceção é lancada
     if not isinstance(message, str):
         raise TypeError("tipo inválido para message")
 
+    # Se key não for um índice positivo válido de message,
+    # retorna a string message invertida
     if key not in range(1, len(message)):
         return "".join(reversed(message))
 
+    # Se key for ímpar:
+    # divide message no índice key, inverte os caracteres de cada parte,
+    # e retorna a união das partes novamente com "_" entre elas
+
+    # Se key for par:
+    # divide message no índice key, inverte a posição das partes,
+    # inverte os caracteres de cada parte, e retorna a união das partes
+    # novamente com "_" entre elas
     part_one = reversed(message[:key])
     part_two = reversed(message[key:])
 
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,31 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    # Se key não possui o tipo correto, uma exceção é lancada
+    # TypeError("tipo inválido para key")
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("123456", "3")
+
+    # Se message não possui o tipo correto, uma exceção é lancada
+    # TypeError("tipo inválido para message")
+    with pytest.raises(TypeError, "tipo inválido para message"):
+        encrypt_message(123456, 3)
+
+    # Se key não for um índice positivo válido de message,
+    # retorna a string message invertida
+    assert encrypt_message("123456", -1) == "654321"  # índice negativo
+    assert encrypt_message("123456", 10) == "654321"  # índice posicao invalida
+
+    # Se key for ímpar:
+    # divide message no índice key, inverte os caracteres de cada parte,
+    # e retorna a união das partes novamente com "_" entre elas
+    assert encrypt_message("123456", 3) == "321_654"
+
+    # Se key for par:
+    # divide message no índice key, inverte a posição das partes,
+    # inverte os caracteres de cada parte, e retorna a união das partes
+    # novamente com "_" entre elas
+    assert encrypt_message("123456", 4) == "65_4321"
