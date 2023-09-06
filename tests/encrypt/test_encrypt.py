1d3cf9a1726b3811f4d60e8860833c4a9945a487
cunhavcs <cunha.vcs@gmail.com>
2023-09-06 10:46:57 -0300

    Req 02: corrige caso de teste

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,31 +1,22 @@
 import pytest
-
 from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    # Se key não possui o tipo correto, uma exceção é lancada
-    # TypeError("tipo inválido para key")
+    pass
+    # key não é do tipo int
     with pytest.raises(TypeError, match="tipo inválido para key"):
-        encrypt_message("ABCDEF", "3")
+        encrypt_message("abcde", "3")
 
-    # Se message não possui o tipo correto, uma exceção é lancada
-    # TypeError("tipo inválido para message")
+    # message não é do tipo str
     with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(3, 3)
 
-    # Se key não for um índice positivo válido de message,
-    # retorna a string message invertida
-    assert encrypt_message("ABCDEF", -1) == "FEDCBA"  # índice negativo
-    assert encrypt_message("ABCDEF", 10) == "FEDCBA"  # índice posicao invalida
+    # key não é um índice válido
+    assert encrypt_message("abcde", 5) == "edcba"
 
-    # Se key for ímpar:
-    # divide message no índice key, inverte os caracteres de cada parte,
-    # e retorna a união das partes novamente com "_" entre elas
-    assert encrypt_message("ABCDEF", 3) == "CBA_FED"
+    # key é par
+    assert encrypt_message("abcde", 2) == "edc_ba"
 
-    # Se key for par:
-    # divide message no índice key, inverte a posição das partes,
-    # inverte os caracteres de cada parte, e retorna a união das partes
-    # novamente com "_" entre elas
-    assert encrypt_message("ABCDEF", 4) == "FE_DCBA"
+    # key é ímpar
+    assert encrypt_message("acb", 1) == "a_bc"
