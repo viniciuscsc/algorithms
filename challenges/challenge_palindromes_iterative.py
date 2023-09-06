e889fa7f327f4cafda32fd88d199291997ceb1df
cunhavcs <cunha.vcs@gmail.com>
2023-09-06 11:55:16 -0300

    Req 06: implementa a funcao is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # Se for passado uma string vazia, retorne False
+    if len(word) == 0:
+        return False
+
+    word_reversed = ""
+
+    for index in range(len(word) - 1, -1, -1):
+        word_reversed += word[index]
+
+    if word == word_reversed:
+        return True
+    else:
+        return False
